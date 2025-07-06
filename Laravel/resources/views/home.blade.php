0c39aec4d33a73da13b595e43b6241d3fd9baaf1
Mostafa Said <m.ssaid356@gmail.com>
2025-07-06 17:00:39 +0300

    Add files via upload

--- Laravel/resources/views/home.blade.php
+++ Laravel/resources/views/home.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'Welcome')
 
 @section('content')
-<div class="container text-center py-5">
-    <h1 class="display-4">Welcome to Our Store</h1>
-    <p class="lead">Discover amazing deals from multiple vendors.</p>
-    <a href="{{ route('shop') }}" class="btn btn-primary btn-lg mt-3">Shop Now</a>
+<div class="flex items-center justify-center h-full">
+    <div class="text-center">
+        <h1 class="text-4xl font-bold text-gray-800">Welcome to MyBrand</h1>
+        <p class="mt-4 text-lg text-gray-600">Discover our newest products and offers</p>
+    </div>
 </div>
 @endsection
