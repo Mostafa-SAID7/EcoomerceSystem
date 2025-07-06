63b730e210a97748799a0060df80e2a20c0e8be3
Mostafa Said <m.ssaid356@gmail.com>
2025-07-06 17:00:39 +0300

    Add files via upload

--- Laravel/resources/views/layouts/app.blade.php
+++ Laravel/resources/views/layouts/app.blade.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'eCommerce')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body class="d-flex flex-column min-vh-100">
     @include('partials.header')
