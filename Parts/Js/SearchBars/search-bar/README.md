3d915ef7690c3745bfa0859df634aea46c6d7e22
Mostafa Said <m.ssaid356@gmail.com>
2025-08-14 01:36:08 +0300

    Add files via upload

---
+++ Parts/Js/SearchBars/search-bar/README.md
@@ -0,0 +1,2 @@
+# search-bar
+This project provides a simple solution to fetch fake data for testing search bar functionality in an e-commerce store. It generates random product names, categories, and descriptions to simulate search queries, allowing developers to test and optimize the search experience without the need for real data.
---
+++ Parts/Js/SearchBars/search-bar/script.js
@@ -0,0 +1,16 @@
+const search = () => {
+  const searchBox = document.getElementById("search-item").value.toUpperCase();
+  const storeitems = document.getElementById("product-list");
+  const product = document.querySelectorAll(".product");
+  const pname = storeitems.getElementsByTagName("h2");
+
+  for (let i = 0; i < pname.length; i++) {
+    let match = product[i].getElementsByTagName("h2")[0];
+    if (match) {
+       let textValue= match.textContent || match.innerHTML
+       if (textValue.toLocaleUpperCase().indexOf(searchBox)> -1) {
+        product[i].style.display=""
+       }else{product[i].style.display="none"}
+    }
+  }
+};
